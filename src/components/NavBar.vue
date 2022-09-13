2e0f50bfbfc0468fc46c985ed5ad30b51f8c4c59
diphokoo <diphokoo@outlook.com>
2022-09-13 17:01:50 +0200

    navBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,20 @@
 <template>
-    <div>
-        <div class="nav-menu">
-            <i class="fas fas-bars" @click="showMenu()"></i>
-
-            <div class="nav-content" :class="this.showMobileMenu ? 'open-menu' : 'closed-menu'">
-                <div class="logo">Night Life</div>
-
-                <ul class="nav-items">
-                    <li>Home</li>
-                    <li>galary</li>
-                    <li>contact</li>
-                </ul>
-
-                <div class="login-button">Login</div>
-
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Night Life</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Galary</a>
+                    <a class="nav-link" href="#">Contact</a>
+                    <a class="nav-link border border-primary rounded-pill" href="#">Login</a>
+                </div>
             </div>
         </div>
-    </div>
+    </nav>
 </template>
 
 <script>

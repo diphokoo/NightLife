d9c935d7b1d76589c959f2cd5f6b4fb64336d94a
diphokoo <diphokoo@outlook.com>
2022-09-21 12:00:54 +0200

    ritgh arrow

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="headerColor">
-        <img id="imgHeader" src="./assests/kabzanPhori-PhotoRoom.png"> 
+        <img id="imgHeader" src="./assests/kabzanPhori-PhotoRoom.png">
+        <i class="fa-solid fa-chevron-right"></i>
     </div>
 </template>
 
@@ -9,8 +10,21 @@
         /* left: 24px; */
         position: absolute;
         margin-left: 135px;
-        margin-top: -23px;
+        margin-top: -22px;
     }
+
+    i.fa-solid.fa-chevron-right[data-v-c970699f] {
+        position: absolute;
+        width: 40px;
+        height: 20px;
+        left: 1264px;
+        top: 295px;
+        font-size: 60px;
+        border: 2.33333px solid #3D37F1;
+        color: #3D37F1;
+        /* transform: rotate(-90deg); */
+    }
+        
     </style>
 <script>
     export default {

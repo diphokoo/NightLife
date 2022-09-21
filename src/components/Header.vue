98156a2863820cde031bd514636c5c25eeeeaef9
diphokoo <diphokoo@outlook.com>
2022-09-21 10:25:02 +0200

    background image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="headerColor">
-        
+        <img id="imgHeader" src="./assests/kabzanPhori-PhotoRoom.png"> 
     </div>
 </template>
+
+<style scoped>
+    img#imgHeader {
+        /* left: 24px; */
+        position: absolute;
+        margin-left: 135px;
+        margin-top: -23px;
+    }
+    </style>
 <script>
     export default {
         name: 'Header',

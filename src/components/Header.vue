2e8bad933ef1332498c7dbdcd1c25cb1f5338a4f
diphokoo <diphokoo@outlook.com>
2022-09-21 12:27:22 +0200

    left arrow

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
     <div class="headerColor">
         <img id="imgHeader" src="./assests/kabzanPhori-PhotoRoom.png">
         <i class="fa-solid fa-chevron-right"></i>
+        <i class="fa-solid fa-chevron-left"></i>
     </div>
 </template>
 
@@ -13,16 +14,26 @@
         margin-top: -22px;
     }
 
-    i.fa-solid.fa-chevron-right[data-v-c970699f] {
+    i.fa-solid.fa-chevron-right {
         position: absolute;
         width: 40px;
         height: 20px;
         left: 1264px;
         top: 295px;
         font-size: 60px;
-        border: 2.33333px solid #3D37F1;
         color: #3D37F1;
         /* transform: rotate(-90deg); */
+    }
+
+    i.fa-solid.fa-chevron-left {
+        position: absolute;
+        width: 40px;
+        height: 20px;
+        left: 90px;
+        top: 255px;
+        font-size: 60px;
+        color: #EDA046;
+        /* transform: rotate(90deg); */
     }
         
     </style>

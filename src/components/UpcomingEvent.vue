bf19fa5e170dad5f01ede557f39aca8c26a7b3da
diphokoo <diphokoo@outlook.com>
2022-10-04 15:27:11 +0200

    setting component

---
+++ src/components/UpcomingEvent.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>this works</p>
+</template>
+
+<script>
+    export default {
+        name: 'UpcomingEvent',
+        props: {
+            UpcomingEvent: Array,
+        }
+    }
+</script>

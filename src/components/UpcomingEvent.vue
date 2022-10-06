1aeac378c39b9195b35ea836234334253d39309b
diphokoo <diphokoo@outlook.com>
2022-10-06 07:25:50 +0200

    event posters

--- src/components/UpcomingEvent.vue
+++ src/components/UpcomingEvent.vue
@@ -33,7 +33,106 @@
         </ul>
     </div>
     </nav>
+
+    <div class="cards row">
+        <div class="card col-5" style="width: 18rem;">
+            <img src="./assests/poster1.jpg" class="card-img-top" alt="...">
+            <div class="card-body row">
+                <div class="col-sm-4">
+                    <span class="month">APR</span>
+                    <h5 class="date">14</h5>
+                </div>
+                <div class="col-sm-8">
+                    <p class="cardP1">Wonder Girls 2010 Wonder <br>Girls World Tour San Francisco</p>
+                        <br>
+                        <br>
+                    <p class="cardP2">We’ll get you directly seated and <br>inside for you to enjoy the show.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card col-5" style="width: 18rem;">
+            <img src="./assests/poster2.jpg" class="card-img-top" alt="...">
+            <div class="card-body row">
+                <div class="col-sm-4">
+                    <span class="month">Aug</span>
+                    <h5 class="date">20</h5>
+                </div>
+                <div class="col-sm-8">
+                    <p class="cardP1">Wonder Girls 2010 Wonder <br>Girls World Tour San Francisco</p>
+                        <br>
+                        <br>
+                    <p class="cardP2">We’ll get you directly seated and <br>inside for you to enjoy the show.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="card col-5" style="width: 18rem;">
+            <img src="./assests/poster3.jfif" class="card-img-top" alt="...">
+            <div class="card-body row">
+                <div class="col-sm-4">
+                    <span class="month">SEP</span>
+                    <h5 class="date">18</h5>
+                </div>
+                <div class="col-sm-8">
+                    <p class="cardP1">Wonder Girls 2010 Wonder <br>Girls World Tour San Francisco</p>
+                        <br>
+                        <br>
+                    <p class="cardP2">We’ll get you directly seated and <br>inside for you to enjoy the show.</p>
+                </div>
+            </div>
+        </div>
+        <div class="card col-5" style="width: 18rem;">
+            <img src="./assests/poster4.jpg" class="card-img-top" alt="...">
+            <div class="card-body row">
+                <div class="col-sm-4">
+                    <span class="month">APR</span>
+                    <h5 class="date">14</h5>
+                </div>
+                <div class="col-sm-8">
+                    <p class="cardP1">Wonder Girls 2010 Wonder <br>Girls World Tour San Francisco</p>
+                        <br>
+                        <br>
+                    <p class="cardP2">We’ll get you directly seated and <br>inside for you to enjoy the show.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card col-5" style="width: 18rem;">
+            <img src="./assests/poster5.jpg" class="card-img-top" alt="...">
+            <div class="card-body row">
+                <div class="col-sm-4">
+                    <span class="month">AUG</span>
+                    <h5 class="date">20</h5>
+                </div>
+                <div class="col-sm-8">
+                    <p class="cardP1">Wonder Girls 2010 Wonder <br>Girls World Tour San Francisco</p>
+                        <br>
+                        <br>
+                    <p class="cardP2">We’ll get you directly seated and <br>inside for you to enjoy the show.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card col-5" style="width: 18rem;">
+            <img src="./assests/poster6.jpg" class="card-img-top" alt="...">
+            <div class="card-body row">
+                <div class="col-sm-4">
+                    <span class="month">SEP</span>
+                    <h5 class="date">18</h5>
+                </div>
+                <div class="col-sm-8">
+                    <p class="cardP1">Wonder Girls 2010 Wonder <br>Girls World Tour San Francisco</p>
+                        <br>
+                        <br>
+                    <p class="cardP2">We’ll get you directly seated and <br>inside for you to enjoy the show.</p>
+                </div>
+            </div>
+        </div>
+    </div>
  </div>
+
 
 </template>
 
@@ -73,6 +172,77 @@ ul.navbar-nav {
     /* margin-right: -315px; */
     padding-left: 530px;
 }
+
+.card.col-5 {
+    margin-left: 30px;
+    margin-bottom: 10px;
+    padding-bottom: 25px;
+}
+
+.cards.row {
+    padding-left: 140px;
+}
+
+.month {
+    position: absolute;
+    width: 25.59px;
+    height: 15.16px;
+
+    font-family: 'DM Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 11.3717px;
+    line-height: 15px;
+    text-align: center;
+
+    color: #3D37F1;
+}
+
+.date {
+    position: absolute;
+    width: 28.43px;
+    height: 36.96px;
+    margin-top: 10px;
+    font-family: 'DM Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 28.4293px;
+    line-height: 37px;
+
+    color: #000000;
+
+}
+
+.cardP1 {
+    position: absolute;
+    width: 241.65px;
+    height: 45.49px;
+
+    font-family: 'DM Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 150%;
+    margin-left: -53px;
+    color: #000000;
+}
+
+.cardP2 {
+    position: absolute;
+    width: 241.65px;
+    height: 39.8px;
+
+    font-family: 'DM Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 150%;
+    margin-left: -53px;
+
+
+    color: #6A6A6A;
+    }
+
 </style>
 
 <script>

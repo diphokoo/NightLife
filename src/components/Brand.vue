20b3aff77176aa92e9f48ebb0f62052db858d599
diphokoo <diphokoo@outlook.com>
2022-10-08 20:17:10 +0200

    brand paragrah

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="brand">
         <h3 class="brandH3">Our partners</h3>
+        <div class="brandContent">
+            <p>
+                We've had the pleasure of working with industry-defining brands. These
+                <br>
+                are just some of them.
+            </p>
+        </div>
     </div>
 </template>
 <script>

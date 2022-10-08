34dee93d28cfd9d722eb324ec2d47f96e3c01dd6
diphokoo <diphokoo@outlook.com>
2022-10-08 20:49:39 +0200

    BrandList

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -8,6 +8,43 @@
                 are just some of them.
             </p>
         </div>
+        <div class="BrandList">
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-2 col-md-6">
+                    <img src="./assests/Brands/image_20.png" class="brandImg">
+                </div>
+
+                <div class="col-lg-2 col-md-6">
+                    <img src="./assests/Brands/image_19.png" class="brandImg" style="margin-top: 5px;">
+                </div>
+
+                <div class="col-lg-2 col-md-6">
+                    <img src="./assests/Brands/image_18.png" class="brandImg" style="margin-top: 15px;">
+                </div>
+
+                <div class="col-lg-2 col-md-6">
+                    <img src="./assests/Brands/image_17.png" class="brandImg" style="margin-top: -10px;">
+                </div>
+
+                <div class="col-lg-2 col-md-6">
+                    <img src="./assests/Brands/image_16.png" class="brandImg" style="margin-top: -36px; margin-left: 60px;">
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center secondRow">
+                <div class="col-sm-3 col-md-3">
+                    <img src="./assests/Brands/image_15.png" class="brandImg" style="margin-top: -35px;">
+                </div>
+                <div class="col-sm-3 col-md-3">
+                    <img src="./assests/Brands/image_14.png" class="brandImg">
+                </div>
+                <div class="col-sm-3 col-md-3">
+                    <img src="./assests/Brands/image_5.png" class="brandImg">
+                </div>
+                <div class="col-sm-3 col-md-3">
+                    <img src="./assests/Brands/image_4.png" class="brandImg">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>

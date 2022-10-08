c4a2ff745947b6b983500c15a0a4cbd90a73829a
diphokoo <diphokoo@outlook.com>
2022-10-08 21:17:18 +0200

    setting up blog page

---
+++ src/components/Blog.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="blog">
+        <p>this works</p>
+    </div>
+</template>
+
+<script>
+        export default {
+        name: 'Blog',
+        props: {
+            Blog: Array,
+        }
+    }
+</script>

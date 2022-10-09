32ac950635be05a3db2b6bcc82bb808f26981643
diphokoo <diphokoo@outlook.com>
2022-10-09 16:47:56 +0200

    logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Night Life</a>
+            <img :src="logo" class="navbar-brand">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -28,7 +28,8 @@
             navHome: "Home",
             navGalary: "Galary",
             navContact: "Contact",
-            navLogin: "Login"
+            navLogin: "Login",
+            logo: "https://db3pap002files.storage.live.com/y4myssw1DRIDem4QyfvlnK-0I3Q4hzH0KGTlqhs75oO5KxgNUzBEze94SmSPcC7b2V_dZKP9tqKv6limPCNuCIWrItv8oNhSkvbpsc63CcWetS74212Cvx3g9oAIEWyJT7u_nRrbCdQnFYmlspjFu13QaJdr7lbRNPJ4ZnoKsRhCgqNTIUpVN3o3qoezd89DyUp?width=225&height=58&cropmode=none"
         }
     }
  }

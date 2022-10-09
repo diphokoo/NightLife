8d98ddc1472fa18cb9ccc1b078a6c7a4fd558c98
diphokoo <diphokoo@outlook.com>
2022-10-09 16:20:21 +0200

    text interpolation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,10 +7,10 @@
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="#">Galary</a>
-                    <a class="nav-link" href="#">Contact</a>
-                    <a class="nav-link border border-primary rounded-pill" href="#">Login</a>
+                    <a class="nav-link" href="#">{{ navHome }}</a>
+                    <a class="nav-link" href="#">{{ navGalary }}</a>
+                    <a class="nav-link" href="#">{{ navContact }}</a>
+                    <a class="nav-link border border-primary rounded-pill" href="#">{{ navLogin }}</a>
                 </div>
             </div>
         </div>
@@ -22,6 +22,14 @@
     name: 'NavBar',
     props: {
         nav: Array,
+    },
+    data() {
+        return {
+            navHome: "Home",
+            navGalary: "Galary",
+            navContact: "Contact",
+            navLogin: "Login"
+        }
     }
  }
 </script>

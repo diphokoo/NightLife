dbcdaa085b11dc04d81963aab8dc72ab8b95bfdd
diphokoo <diphokoo@outlook.com>
2022-10-21 23:55:03 +0200

    blog

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,48 +1,22 @@
 <template>
     <div class="blog">
-        <h3 class="blogH3">Blog</h3>
+        <h3 class="blogH3">{{ blogHeader }}</h3>
         <div class="blogContent">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                {{ blogP }}
             </p>
         </div>
         <div class="row blogCards">
             <div class="row">
-                <div class="col-sm-4">
+                <div class="col-sm-4" v-for="blog in blogs" :key="blogs.id">
                     <div class="card" style="width: 18rem;">
-                        <img src="./assests/Blog/Image_1.png" class="card-img-top">
+                        <img :src="blog.cardimage" class="card-img-top">
                         <div class="card-body">
                             <h5 class="card-title">
-                                6 Strategies to Find Your Conference Keynote and Other Speakers
+                                {{ blog.cardtitle }}
                             </h5>
                             <p class="card-text">
-                                Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.
-                            </p>
-                        </div>
-                    </div>
-                                    </div>
-                <div class="col-sm-4">
-                    <div class="card" style="width: 18rem;">
-                        <img src="./assests/Blog/Image_2.png" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                How Successfully Used Paid Marketing to Drive Incremental Ticket Sales
-                            </h5>
-                            <p class="card-text">
-                                Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="card" style="width: 18rem;">
-                        <img src="./assests/Blog/Image_3.png" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                Introducing Workspaces: Work smarter, not harder with new navigation
-                            </h5>
-                            <p class="card-text">
-                                Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.
+                                {{ blog.cardtext }}
                             </p>
                         </div>
                     </div>
@@ -59,6 +33,33 @@
         name: 'Blog',
         props: {
             Blog: Array,
+        },
+        data() {
+            return {
+                blogHeader: 'Blog',
+                blogP: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+                blogs: [
+                    {
+                        id: 1,
+                        cardimage: './assests/Blog/Image_1.png',
+                        cardtitle: '6 Strategies to Find Your Conference Keynote and Other Speakers',
+                        cardtext: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.'
+
+                    },
+                    {
+                        id: 2,
+                        cardimage: './assests/Blog/Image_2.png',
+                        cardtitle: 'How Successfully Used Paid Marketing to Drive Incremental Ticket Sales',
+                        cardtext: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.'
+                    },
+                    {
+                        id: 3,
+                        cardimage: './assests/Blog/Image_3.png',
+                        cardtitle: 'Introducing Workspaces: Work smarter, not harder with new navigation',
+                        cardtext: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.'
+                    }
+                ]
+            }
         }
     }
 </script>
